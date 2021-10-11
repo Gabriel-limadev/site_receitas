72e33e06f92fd2753244acfe5db940a0c790f394
Gabriel Lima <gabrielvitor180701@gmail.com>
2021-10-11 13:02:47 -0300

    Adicionando o repositório de templates

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,6 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Receitas</h1>')
+    return render(request,'index.html')
